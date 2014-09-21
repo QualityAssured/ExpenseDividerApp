18bf983eba69c65997534ad50fba7e4a3902a5c7
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-09-21 23:26:00 +0200

    Selected tags appear on the main screen

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+        var selectedTags = [];
+
+        $('#selected_tags_container').hide();
+
         $('#add_tags').click(function() {
             $('#tags_container').toggleClass('open');
             $('#bill_input').toggleClass('open');
@@ -10,6 +14,28 @@ $(document).ready(function() {
         })
 
         $('.tag').click(function() {
+            value = $(this).val();
+            index = $.inArray(value, selectedTags)
+            previousLength = selectedTags.length;
             $(this).toggleClass('selected btn-primary');
+            if( index > -1) {
+                selectedTags.splice(index, 1);
+            }
+            else {
+                selectedTags.push(value);
+            }
+
+            if(selectedTags.length > 0) {
+                $('#selected_tags_container').show();
+                if(selectedTags.length > previousLength){
+                    $(this).clone().appendTo('#selected_tags_container');
+                }
+                else {
+
+                }
+            }
+            else {
+                $('#selected_tags_container').hide();
+            }
         })
-    });
+});
