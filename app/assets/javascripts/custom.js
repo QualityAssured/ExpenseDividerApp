0b33e9a176712a4e036afe6027e2c0c731eb1db8
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-09-23 22:09:13 +0200

    Added some number fields

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -15,27 +15,20 @@ $(document).ready(function() {
 
         $('.tag').click(function() {
             value = $(this).val();
-            index = $.inArray(value, selectedTags)
-            previousLength = selectedTags.length;
+            index = $.inArray(value, selectedTags);
+
             $(this).toggleClass('selected btn-primary');
             if( index > -1) {
                 selectedTags.splice(index, 1);
+                $("#selected_tags_container input[value=" + $(this).val() + "]").remove();
+                $("." + $(this).val()).next("br").remove();
+                $("." + $(this).val()).remove();
             }
             else {
                 selectedTags.push(value);
-            }
-
-            if(selectedTags.length >= 0) {
                 $('#selected_tags_container').show();
-                if(selectedTags.length > previousLength){
-                    $(this).clone().appendTo('#selected_tags_container');
-                }
-                if(selectedTags.length < previousLength) {
-                    $("#selected_tags_container input[value=" + $(this).val() + "]").remove();
-                }
-            }
-            else {
-                $('#selected_tags_container').hide();
+                $(this).clone().appendTo('#selected_tags_container');
+                $('#selected_tags_container').append('<input class=' + $(this).val() + ' type="number" /><i class="fa fa-times ' + $(this).val() + '"></i></br>');
             }
         })
 });
