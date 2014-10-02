a85878eae40ad6a0a6dea05bb84cd7c6ab11473b
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-10-02 22:46:12 +0200

    Saving bills

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -5,6 +5,10 @@ class BillsController < ApplicationController
   end
 
   def create
+
+    tags = params[:tagswithamount].to_s
+    Bill.create(user_id: current_user.id, total: params[:total], tags: tags)
+
     render :created
   end
 
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,4 @@
 class Bill < ActiveRecord::Base
 
-attr_accessor :user_id, :total, :tags
+
 end
