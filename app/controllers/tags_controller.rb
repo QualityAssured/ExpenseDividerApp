4ec37c149321f9742e7a80f012e3a96c2215574f
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-10-13 18:59:57 +0200

    Added TODO

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,6 +10,8 @@ class TagsController < ApplicationController
   end
 
   def destroy_multiple
+    #TODO: Investigate if this could be dangerous
+    # If a user fakes a post, with a tag id that does not belong to him, will it destroy another user's tag?
     Tag.destroy(params[:tag_ids].split(','))
     render :new
   end
