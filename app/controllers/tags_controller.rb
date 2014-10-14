d87f3c7c6227644cdf1b69b9fc16b8aaefda56c0
Alain Thoen <alain.thoen@protime.be>
2014-10-14 15:28:03 +0200

    check current_user with tag owner

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,9 +10,13 @@ class TagsController < ApplicationController
   end
 
   def destroy_multiple
-    #TODO: Investigate if this could be dangerous
-    # If a user fakes a post, with a tag id that does not belong to him, will it destroy another user's tag?
-    Tag.destroy(params[:tag_ids].split(','))
+    tags_to_destroy = Tag.find(params[:tag_ids].split(','))
+    tags_to_destroy.each do |t|
+      if current_user.id == t.user_id
+        t.destroy
+      end
+    end
+    # Tag.destroy(params[:tag_ids].split(','))
     render :new
   end
 
