f228645d09b17bb191e1a5c17f51893567b96683
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-10-28 16:38:06 +0100

    Removing tags from new bills page works again

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -20,9 +20,8 @@ $(document).ready(function() {
             $(this).toggleClass('selected btn-primary','#bill_input');
             if( index > -1) {
                 selectedTags.splice(index, 1);
-                $("#selected_tags_container input[value=" + $(this).val() + "]",'#bill_input').remove();
-                $("." + $(this).val()).next("br").remove();
-                $("." + $(this).val()).remove();
+                tag_name = $(this).val();
+                removeLine(tag_name);
             }
             else {
                 selectedTags.push(value);
@@ -45,6 +44,16 @@ function addLine(tag_value, default_user_id) {
     button.value = tag_value;
     number_field.name = buildNameFromValueAndId(tag_value, default_user_id);
     line.appendTo('#selected_tags_container','#bill_input');
+}
+
+function removeLine(tag_name) {
+    line[0] = $("#selected_tags_container input[value='" + tag_name + "']");
+    line[1] = line[0].next();
+    line[2] = line[1].next();
+    line[3] = line[2].next();
+    for (var i = 3; i > -1; i--) {
+        line[i].remove();
+    }
 }
 
 function buildNameFromValueAndId(tag_value, id) {
