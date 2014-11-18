761c2d210d5e688d558f7b9d45f9754231912000
Tom Lauwyck <tom.lauwyck@gmail.com>
2014-11-18 19:06:09 +0100

    Fixed all deprecation warnings in specs

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -5,23 +5,23 @@ RSpec.describe Bill, :type => :model do
   describe "Bill" do
 
     it "should have a valid factory" do
-      FactoryGirl.build(:bill, :group_id => "1").should be_valid
+      expect(FactoryGirl.build(:bill, :group_id => "1")).to be_valid
     end
 
     it "validates user_id is present" do
-      FactoryGirl.build(:bill, :user_id => "").should_not be_valid
+      expect(FactoryGirl.build(:bill, :user_id => "")).to_not be_valid
     end
 
     it "validates description is present" do
-      FactoryGirl.build(:bill, :description => "").should_not be_valid
+      expect(FactoryGirl.build(:bill, :description => "")).to_not be_valid
     end
 
     it "validates total is present" do
-      FactoryGirl.build(:bill, :total => "").should_not be_valid
+      expect(FactoryGirl.build(:bill, :total => "")).to_not be_valid
     end
 
     it "validates group_id is present" do
-      FactoryGirl.build(:bill, :group_id => "").should_not be_valid
+      expect(FactoryGirl.build(:bill, :group_id => "")).to_not be_valid
     end
   end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -5,20 +5,21 @@ RSpec.describe Group, :type => :model do
   describe "Group" do
 
     it "should have a valid factory" do
-      FactoryGirl.build(:group).should be_valid
+      expect(FactoryGirl.build(:group)).to be_valid
     end
 
     it "validates owner_id is present" do
-      FactoryGirl.build(:group, :owner_id => "").should_not be_valid
+      expect(FactoryGirl.build(:group, :owner_id => "")).to_not be_valid
     end
 
     it "validates group_name is present" do
-      FactoryGirl.build(:group, :group_name => "").should_not be_valid
+      expect(FactoryGirl.build(:group, :group_name => "")).to_not be_valid
     end
 
     it "validates group_name is unique" do
-      FactoryGirl.create(:group, :group_name => "bla", :owner_id => 1).should be_valid
-      FactoryGirl.build(:group,:group_name => "bla", :owner_id => 1).should_not be_valid
+      expect(FactoryGirl.create(:group, :group_name => "bla", :owner_id => 1)).to be_valid
+      expect(FactoryGirl.build(:group,:group_name => "bla", :owner_id => 1)).to_not be_valid
+
     end
 
   end
